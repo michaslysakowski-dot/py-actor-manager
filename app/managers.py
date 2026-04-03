53e84797fa50b7f961061a3a0ebff95eb6fe41d2
Michal Lysakowski <michaslysakowski@gmail.com>
2026-04-03 12:39:01 +0200

    Solution

--- app/managers.py
+++ app/managers.py
@@ -3,4 +3,53 @@ import sqlite3
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name, table_name):
+        self.connection = sqlite3.connect(db_name)
+        self.table_name = table_name
+        self.actor_list = []
+
+    con = sqlite3.connect(":memory:")
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE actor "
+                   "(id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT)")
+    con.commit()
+    con.close()
+
+    def create(self, first_name, last_name):
+        query = (
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            f"VALUES (?, ?)"
+        )
+        cursor = self.connection.cursor()
+        cursor.execute(query, (first_name, last_name))
+        self.connection.commit()
+
+    def update(self, pk, new_first_name, new_last_name):
+        query = (
+            f"UPDATE {self.table_name} "
+            f"SET first_name = ?, last_name = ? "
+            f"WHERE id = ?"
+        )
+        cursor = self.connection.cursor()
+        cursor.execute(query, (new_first_name, new_last_name, pk))
+        self.connection.commit()
+
+    def delete(self, pk) -> None:
+        query = f"DELETE FROM {self.table_name} WHERE id = ?"
+        cursor = self.connection.cursor()
+        cursor.execute(query, (pk,))
+        self.connection.commit()
+
+    def all(self) -> list[Actor]:
+        query = f"SELECT id, first_name, last_name FROM {self.table_name}"
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+
+        actors = []
+        for row in rows:
+            actor = Actor(id=row[0], first_name=row[1], last_name=row[2])
+            actors.append(actor)
+
+        return actors
